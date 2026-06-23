513da7b0d8ed15b7e3ce10650dbe35cef5cd7d97
Manasagundavarapu <gundavarapumanasa@gmail.com>
2026-06-23 09:28:26 +0530

    Create findContentChildren.py

---
+++ StriversSDESheet/findContentChildren.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = 0  # child pointer
+        j = 0  # cookie pointer
+
+        while i < len(g) and j < len(s):
+
+            if s[j] >= g[i]:
+                i += 1      # child is satisfied
+
+            j += 1          # cookie used
+
+        return i
