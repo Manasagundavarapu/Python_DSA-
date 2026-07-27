7419ec858ba7dd0cef854fe376371616d2f04a39
Manasagundavarapu <gundavarapumanasa@gmail.com>
2026-07-27 10:49:41 +0530

    Create validateStackSequences.py

---
+++ StriversSDESheet/validateStackSequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack=[]
+        j=0
+
+        for ele in pushed:
+            stack.append(ele)
+
+            while stack and stack[-1]==popped[j]:
+                stack.pop()
+                j+=1
+        return len(stack)==0
